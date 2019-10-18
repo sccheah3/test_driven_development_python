1fab7343c72a9b0600376029b0385a73e4d6f7b4
Stanley Cheah <sccheah3@gmail.com>
2019-10-18 16:55:24 -0700

    test POST new todo

--- lists/tests.py
+++ lists/tests.py
@@ -6,14 +6,22 @@ from lists.views import home_page
 
 class HomePageTest(TestCase):
 
-    def test_home_page_returns_correct_html(self):
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+
+    # def test_home_page_returns_correct_html(self):
     #    request = HttpRequest()         # we create an 'HttpRequest' object, which is what
                                         #   Django will see when a user's browser asks for a page
     #    response = home_page(request)   # Pass it to our home_page view, which gives us a response.
                                         #   This object is an instance of a class HttpResponse
 
-        response = self.client.get('/')
-        self.assertTemplateUsed(response, 'home.html')
+    #    response = self.client.get('/')
+    #    self.assertTemplateUsed(response, 'home.html')
 
         # html = response.content.decode('utf8')  # Extract the .content of the response. These are
                                                 #   raw bytes. We call .decode() to convert them into 
