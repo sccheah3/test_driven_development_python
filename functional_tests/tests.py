c50bc4742551137874470dc33b1343876b13289d
Stanley Cheah <sccheah3@gmail.com>
2019-10-23 17:42:25 -0700

    remove static wait time for element to load; max is 10 sec interval is .5

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,6 +1,7 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 
 import time
 # import unittest
@@ -30,7 +31,7 @@ import time
 #	the browser has finished reloading before we make any assertions about the
 #	new page. This is called an "explicit wait"
 
-
+MAX_WAIT = 10
 
 # class NewVisitorTest(unittest.TestCase):
 class NewVisitorTest(LiveServerTestCase):
@@ -40,10 +41,22 @@ class NewVisitorTest(LiveServerTestCase):
 	def tearDown(self):
 		self.browser.quit()
 
-	def check_for_row_in_list_table(self, row_text):
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn(row_text, [row.text for row in rows])
+	def wait_for_row_in_list_table(self, row_text):
+		start_time = time.time()
+
+		while True:
+			try:
+				table = self.browser.find_element_by_id('id_list_table')
+				rows = table.find_elements_by_tag_name('tr')
+				self.assertIn(row_text, [row.text for row in rows])
+			
+				return
+			except(AssertionError, WebDriverException) as e:
+				if time.time() - start_time > MAX_WAIT:
+					raise e
+
+				time.sleep(0.5)
+
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		# check out homepage
@@ -71,21 +84,17 @@ class NewVisitorTest(LiveServerTestCase):
 		# page will update after entering
 		# page now lists "1: Buy peacock feathers" as an item in todo list
 		inputbox.send_keys(Keys.ENTER)
+		self.wait_for_row_in_list_table('1: Buy peacock feathers') 
 
-		time.sleep(1)
-
-		self.check_for_row_in_list_table('1: Buy peacock feathers') 
 		# There is still a text box inviting her to add another item. She 
 		# enters "Use peacock feathers to make a fly" (Edith is very # methodical) 
 		inputbox = self.browser.find_element_by_id('id_new_item') 
 		inputbox.send_keys('Use peacock feathers to make a fly') 
 		inputbox.send_keys( Keys.ENTER) 
-
-		time.sleep(1) 
 		
 		# The page updates again, and now shows both items on her list 
-		self.check_for_row_in_list_table('1: Buy peacock feathers') 
-		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly') 
+		self.wait_for_row_in_list_table('1: Buy peacock feathers') 
+		self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly') 
 
 
 		# another text box exists still
