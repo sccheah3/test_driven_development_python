09a5c5f3477096206a6959766ea789586ac879ab
Stanley Cheah <sccheah3@gmail.com>
2019-10-24 14:35:51 -0700

    refactor for new design to take multiple user

--- lists/views.py
+++ lists/views.py
@@ -5,16 +5,14 @@ from lists.models import Item
 
 # Create your views here.
 def home_page(request):
-	if request.method == "POST":
-		# new_item_text = request.POST['item_text']
-		# .objects.create is shorthand for creating new Item w/o needing to call .save()
-		Item.objects.create(text=request.POST['item_text'])		
-
-		return redirect('/lists/the-only-list-in-the-world/')
-
 	return render(request, 'home.html')
 	# return render(request, 'home.html', {'items': Item.objects.all()})
 
 def view_list(request):
 	items = Item.objects.all()
 	return render(request, 'list.html', {'items': items})
+
+def new_list(request):
+	if request.method == "POST":
+		Item.objects.create(text=request.POST['item_text'])
+	return redirect('/lists/the-only-list-in-the-world/')
