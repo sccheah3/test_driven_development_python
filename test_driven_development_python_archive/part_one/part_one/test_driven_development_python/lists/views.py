45789b9ed451de390f31f8c7b07ce27986e39055
sccheah3 <52621486+sccheah3@users.noreply.github.com>
2019-11-06 21:25:58 -0800

    Add files via upload

---
+++ test_driven_development_python_archive/part_one/part_one/test_driven_development_python/lists/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+
+from lists.models import Item, List
+
+# Create your views here.
+def home_page(request):
+	return render(request, 'home.html')
+	# return render(request, 'home.html', {'items': Item.objects.all()})
+
+def view_list(request, list_id):
+	# list_ = List.objects.get(id=list_id)
+	# items = Item.objects.filter(list=list_)
+	# return render(request, 'list.html', {'items': items})
+
+	list_ = List.objects.get(id=list_id)
+	return render(request, 'list.html', {'list': list_})
+
+def new_list(request):
+	if request.method == "POST":
+		list_ = List.objects.create()
+		Item.objects.create(text=request.POST['item_text'], list=list_)
+	return redirect(f'/lists/{list_.id}/')
+
+def add_item(request, list_id):
+	list_ = List.objects.get(id=list_id)
+	Item.objects.create(text=request.POST['item_text'], list=list_)
+	return redirect(f'/lists/{list_.id}/')
