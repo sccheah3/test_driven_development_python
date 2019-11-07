e1b61181284aa9fdee69dc6c3d5cd15ba8923994
Stanley Cheah <sccheah3@gmail.com>
2019-11-06 22:56:52 -0800

    first steps of FT for layout and styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -7,6 +7,9 @@ import time
 
 MAX_WAIT = 10
 
+# LiveServerTestCase launches a live Django server in background
+#	on setup and shuts it down on teardown
+
 class NewVisitorTest(LiveServerTestCase):
 	# setup and teardown are executed before and after each test method
 	def setUp(self):
@@ -108,3 +111,28 @@ class NewVisitorTest(LiveServerTestCase):
 		self.assertNotIn('Buy peacock feathers', page_text)
 		self.assertIn('Buy milk', page_text)
 
+
+	def test_layout_and_styling(self):
+		# visit home page
+		self.browser.get(self.live_server_url)
+		self.browser.set_window_size(1024, 768)
+
+		# input box is centered
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertAlmostEqual(
+			inputbox.location['x'] + inputbox.size['width'] / 2,
+			512,
+			delta=10
+		)
+
+
+		# start new list and see the input is nicely centered
+		inputbox.send_keys('testing')
+		inputbox.send_keys(Keys.ENTER)
+		self.wait_for_row_in_list_table('1: testing')
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertAlmostEqual(
+			inputbox.location['x'] + inputbox.size['width'] / 2,
+			512,
+			delta=10
+		)
