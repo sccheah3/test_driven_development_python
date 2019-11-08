a11c48e57a4da3f105f3f6224ec79d668a2acbe7
Stanley Cheah <sccheah3@gmail.com>
2019-11-08 11:34:06 -0800

    set STATIC_ROOT in settings and disable admin

--- superlists/settings.py
+++ superlists/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
+    # 'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -119,3 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Relying on django dev server to serve static files are too slow
+# We can use Apache or Nginx or even use a CDN to load static files
+#   want to have all static files from inside all various apps in one place
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
